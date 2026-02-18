fc077595583b673b4c574e6a0ea728a25193ff41
Anurag <57651580+Anurag6239@users.noreply.github.com>
2026-02-18 15:11:32 +0530

    Python selenium framework

--- pageObjects/logout.py
+++ pageObjects/logout.py
@@ -1,12 +1,16 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 class LogOut:
     def __init__(self, driver):
         self.driver = driver
-        self.dropdown_button = (By.XPATH, "//ul[@class='oxd-dropdown-menu']//li[4]")
+        self.wait = WebDriverWait(self.driver, 15)
+        self.dropdown_button = (By.XPATH, "//span[contains(@class,'oxd-userdropdown-tab')]")
+        self.logout_button = (By.XPATH, "//a[text()='Logout']")
 
     def logout_click(self):
-        self.driver.find_element(*self.dropdown_button).click()
+        self.wait.until(EC.visibility_of_element_located(self.dropdown_button)).click()
+        self.wait.until(EC.element_to_be_clickable(self.dropdown_button)).click()
 
     
